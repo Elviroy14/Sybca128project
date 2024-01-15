1539d96b44706e98c3a9f85e678daffc5743b185
Elviroy14 <rodrigueselviroy14@gmail.com>
2024-01-15 10:33:15 +0530

    commit branch

---
+++ sybca218/src/sybca218/elvioy14.java
@@ -0,0 +1,26 @@
+package sybca218;
+
+import java.util.Scanner;
+
+public class elvioy14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int x,y, sum;
+	Scanner sc = new Scanner (System.in);
+	System.out.println("type a number:");
+	x= sc.nextInt();
+	
+	System.out.println("type a  second number:");
+	y= sc.nextInt();
+		
+	sum = x+y;
+	System.out.println("sum is:"+ sum);
+		
+		
+		
+		
+	}
+
+}
---
+++ sybca218/src/sybca218/nik218.java
@@ -0,0 +1,10 @@
+package sybca218;
+
+public class nik218 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hellow world");
+	}
+
+}
